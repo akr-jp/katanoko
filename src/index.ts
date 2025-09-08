16d777407704e1ee6def890b176a00ae5067744d
AKR <akr5061987@gmail.com>
2025-09-08 14:05:20 +0000

    feat: Add prettier and format code
    - Add .prettierignore and .prettierrc.json
    - Add format script to package.json
    - Format all files with prettier

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
+import { cac } from 'cac';
+import { promises as fs } from 'fs';
+import path from 'path';
 
-import { cac } from "cac";
-import { promises as fs } from "fs";
-import path from "path";
-
-const cli = cac("katanoko");
+const cli = cac('katanoko');
 
 const configTemplate = `import type { Config } from "katanoko";
 
@@ -17,20 +16,20 @@ export default {
 } satisfies Config;
 `;
 
-cli.command("init", "Create a new katanoko.config.ts file").action(async () => {
-  const configPath = path.join(process.cwd(), "katanoko.config.ts");
+cli.command('init', 'Create a new katanoko.config.ts file').action(async () => {
+    const configPath = path.join(process.cwd(), 'katanoko.config.ts');
 
-  try {
-    await fs.writeFile(configPath, configTemplate);
-    console.log("\u001b[32mSuccess[0m katanoko.config.ts created.");
-  } catch (e) {
-    console.error("\u001b[31mError[0m Failed to create katanoko.config.ts.");
-    console.error(e);
-    process.exit(1);
-  }
+    try {
+        await fs.writeFile(configPath, configTemplate);
+        console.log('\u001b[32mSuccess[0m katanoko.config.ts created.');
+    } catch (e) {
+        console.error('\u001b[31mError[0m Failed to create katanoko.config.ts.');
+        console.error(e);
+        process.exit(1);
+    }
 });
 
 cli.help();
-cli.version("0.1.0");
+cli.version('0.1.0');
 
 cli.parse();
